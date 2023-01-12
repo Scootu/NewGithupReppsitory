38b2c3ced2f1a82345bd4bedb932780d21b504b2
scootu <bacetd2004@gmail.com>
2023-01-12 13:13:08 -0500

    fix the function problem

--- function.cpp
+++ function.cpp
@@ -10,5 +10,9 @@ int main() {
   cout << "entre you number " <<endl ;
   cin >> int n ; 
    cout <<"the number *2 is" <<function(n)<<endl ;
+   
+   for(int i = 0 ;i< 100 ; i++) 
+       cout << "the number is :" << function(i) <<endl ; 
+       
 }
   
