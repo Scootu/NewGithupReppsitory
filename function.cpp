77a41ab280e862a98cb5e4be8513e761be69609c
aneshamdaoui <97843566+Scootu@users.noreply.github.com>
2023-01-12 13:07:57 -0500

    Create a function n
    this function for multiple the number * 2 and return the value

---
+++ function.cpp
@@ -0,0 +1,14 @@
+#include <iostream> 
+
+using namespace std ;
+//for multiple *2
+
+int function(int n) { 
+  return n*2 ;
+}
+int main() { 
+  cout << "entre you number " <<endl ;
+  cin >> int n ; 
+   cout <<"the number *2 is" <<function(n)<<endl ;
+}
+  
